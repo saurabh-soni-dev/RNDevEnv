545547719a8245f334c0631bc892ea8f9ce3b8e1
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-05-25 14:58:24 +0530

    Add build commands

---
+++ MakeBuild/BeforeCommands.ts
@@ -0,0 +1,6 @@
+// iOS- 
+// npx react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
+
+// Android - 
+// npx react-native bundle --platform android --dev false --entry-file ./index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res/ --sourcemap-output ./android/app/src/main/assets/index.android.map
+
